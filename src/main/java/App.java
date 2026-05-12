3035178754006d6f4f80527fc6bc5d0fc7a7f55b
navneet <suhaskarnam08@gmail.com>
2026-05-12 15:38:53 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,29 +1,9 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 public class App {
-
-    public static void main(String[] args) {
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-
-        try {
-            driver.get("https://www.saucedemo.com/");
-            driver.findElement(By.id("user-name")).sendKeys("standard_user");
-            driver.findElement(By.id("password")).sendKeys("secret_sauce");
-            driver.findElement(By.id("login-button")).click();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            driver.quit();
-        }
+    public String getGreeting() {
+        return "Hello.How r u?";
     }
+    public static void main(String args[]){
+        System.out.println(new App().getGreeting());
+    }
+
 }
